29e8e97a06178d52643dcf93214a0b3665e5a34c
Andrea Vanini <andrea.vanini@uidu.org>
2019-03-19 00:05:35 +0100

    Fixes and deploy website

--- todo/media-uploader/dist/esm/index.js
+++
@@ -1,5 +0,0 @@
-import { ComponentHOC } from '@uidu/field-base';
-import CheckboxComponent from './Checkbox';
-import CheckboxGroupComponent from './CheckboxGroup';
-export var Checkbox = ComponentHOC(CheckboxComponent);
-export var CheckboxGroup = ComponentHOC(CheckboxGroupComponent);
