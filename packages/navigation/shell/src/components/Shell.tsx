1d24f9229ade8053b93962e71b6c99b816bafc6c
Andrea Vanini <andrea.vanini@uidu.org>
2019-05-03 18:48:16 +0200

    FIX example

--- packages/navigation/shell/src/components/Shell.tsx
+++ packages/navigation/shell/src/components/Shell.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Wrapper } from '../styled';
-
 import { ShellProps } from '../types';
+
 
 export default function Shell({ fixedHeight = true, children }: ShellProps) {
   return <Wrapper fixedHeight={fixedHeight}>{children}</Wrapper>;
