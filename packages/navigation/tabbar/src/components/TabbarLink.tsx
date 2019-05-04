baac403781cf7a1c0e7ceffa4df5cf1c9610f967
Andrea Vanini <andrea.vanini@uidu.org>
2019-05-04 12:08:11 +0200

    Release tabbar

--- packages/navigation/tabbar/src/components/TabbarLink.tsx
+++ packages/navigation/tabbar/src/components/TabbarLink.tsx
@@ -3,11 +3,13 @@ import { NavLink as Link } from 'react-router-dom';
 
 export default class TabbarLink extends PureComponent<any> {
   render() {
+    const { to, ...otherProps } = this.props;
     return (
       <li className="nav-item">
         <Link
+          to={to}
           className="nav-link d-flex flex-column align-items-center justify-content-center py-3"
-          {...this.props}
+          {...otherProps}
         />
       </li>
     );
