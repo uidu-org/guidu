6f428b5ba8fdd5dfea7429d2afb086ce67ee1eb2
Andrea Vanini <andrea.vanini@uidu.org>
2019-07-24 15:31:20 +0200

    FIX #33

--- packages/media/media-filmstrip/src/styled.ts
+++ packages/media/media-filmstrip/src/styled.ts
@@ -1,11 +1,11 @@
 import styled from 'styled-components';
 
 export const FilmStrip = styled.div`
-  overflow-x: 'auto';
-  -webkit-overflow-scrolling: 'touch';
-  white-space: 'nowrap';
-  padding-bottom: '1rem';
-  padding-top: '1rem';
-  margin-top: '-1rem';
-  margin-bottom: '-1rem';
+  overflow-x: auto;
+  -webkit-overflow-scrolling: touch;
+  white-space: nowrap;
+  padding-bottom: 1rem;
+  padding-top: 1rem;
+  margin-top: -1rem;
+  margin-bottom: -1rem;
 `;
