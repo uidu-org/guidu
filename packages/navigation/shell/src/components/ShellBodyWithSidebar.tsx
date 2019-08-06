fd321d776ca80aaead78f3f76f4f7c433fe97af2
Andrea Vanini <andrea.vanini@uidu.org>
2019-08-06 16:54:01 +0200

    FIX typescript error

--- packages/navigation/shell/src/components/ShellBodyWithSidebar.tsx
+++ packages/navigation/shell/src/components/ShellBodyWithSidebar.tsx
@@ -1,23 +1,25 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  display: flex;
+  flex: 1 1 auto;
+  height: 100%;
+  overflow: hidden;
+`;
+
+const Inner = styled.div`
+  -webkit-overflow-scrolling: touch;
+  height: auto;
+  flex: 1 1 auto;
+  scroll-behavior: smooth;
+  overflow-y: scroll;
+  overscroll-behavior-y: contain;
+`;
 
 export default ({ children, sidebar }) => (
-  <div
-    css={{
-      display: 'flex',
-      flex: '1 1 auto',
-      height: '100%',
-      overflow: 'hidden',
-    }}
-  >
-    <div
-      css={{
-        overflow: 'auto',
-        WebkitOverflowScrolling: 'touch',
-        flex: '1 1 auto',
-      }}
-    >
-      {children}
-    </div>
+  <Wrapper>
+    <Inner>{children}</Inner>
     {sidebar}
-  </div>
+  </Wrapper>
 );
