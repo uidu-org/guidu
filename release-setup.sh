3c67ae23ab22169f9445958f98ba77aac3eb203f
Andrea Vanini <andrea.vanini@uidu.org>
2019-08-24 00:04:59 +0200

    Added otp token

--- release-setup.sh
+++ release-setup.sh
@@ -9,7 +9,7 @@ git push https://${GH_TOKEN}@github.com/uidu-org/guidu.git HEAD:master > /dev/nu
 echo -e "\e[32m Setting git configs..."
 yarn build:pkg
 echo -e "\e[32m Setting git configs..."
-yarn changeset release --public
+yarn changeset release --public --otp=$NPM_TOKEN
 echo -e "\e[32m Setting git configs..."
 git push https://${GH_TOKEN}@github.com/uidu-org/guidu.git --tags > /dev/null 2>&1
 echo -e "\e[32m Setting git configs..."
