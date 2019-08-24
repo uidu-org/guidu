1638bdab256b1995674d20b7402f720c91dbd095
Andrea Vanini <andrea.vanini@uidu.org>
2019-08-24 11:11:23 +0200

    Added change

--- packages/dashboards/dashboard-controls/src/components/Navigator.tsx
+++ packages/dashboards/dashboard-controls/src/components/Navigator.tsx
@@ -15,12 +15,12 @@ export default class Navigator extends Component<NavigatorProps> {
 
     return (
       <div className="d-flex align-items-center">
+        <Trigger activeBg="#d0f0fd" className="btn" onClick={onPrev}>
+          <ChevronLeft strokeWidth={2} size={14} />
+        </Trigger>
         <Trigger activeBg="#d0f0fd" className="btn">
           <Calendar strokeWidth={2} size={14} className="mr-2" />
           <span style={{ textTransform: 'initial' }}>{label}</span>
-        </Trigger>
-        <Trigger activeBg="#d0f0fd" className="btn" onClick={onPrev}>
-          <ChevronLeft strokeWidth={2} size={14} />
         </Trigger>
         <Trigger activeBg="#d0f0fd" className="btn" onClick={onNext}>
           <ChevronRight strokeWidth={2} size={14} />
