de77bcb112870f7dc3a819f25a444715181840e5
Andrea Vanini <andrea.vanini@uidu.org>
2019-09-18 09:29:35 +0200

    Export calendar controls

--- packages/data/data-controls/src/index.ts
+++ packages/data/data-controls/src/index.ts
@@ -1,4 +1,10 @@
 export { default as CalendarToolbar } from './components/CalendarToolbar';
+export {
+  default as CalendarNavigator,
+} from './components/CalendarToolbar/CalendarNavigator';
+export {
+  default as CalendarView,
+} from './components/CalendarToolbar/CalendarView';
 export { default as Customizer } from './components/Customizer';
 export { default as Filterer } from './components/Filterer';
 export { default as Finder } from './components/Finder';
