ff48fe45a53a83a96d4b19d873b00ea24f0107a7
Andrea Vanini <andrea.vanini@uidu.org>
2019-09-20 23:26:21 +0200

    Try if helps sideEffects

--- packages/data/table/src/components/Table.tsx
+++ packages/data/table/src/components/Table.tsx
@@ -1,9 +1,6 @@
-import loadable from '@loadable/component';
-import { ShellBody, ShellBodyWithSpinner } from '@uidu/shell';
+import { AgGridReact } from 'ag-grid-react';
 import React, { Component } from 'react';
 import CustomHeader from './headers';
-
-const AgGridReact = (loadable as any).lib(() => import('ag-grid-react'));
 
 export default class Table extends Component<any> {
   static defaultProps = {
@@ -15,59 +12,49 @@ export default class Table extends Component<any> {
     return (
       <div className={`ag-theme-${theme} h-100`}>
         <AgGridReact
-          fallback={
-            <ShellBody>
-              <ShellBodyWithSpinner />
-            </ShellBody>
-          }
-        >
-          {({ AgGridReact: Grid }) => (
-            <Grid
-              ref={innerRef}
-              componentWrappingElement="div"
-              columnDefs={this.props.columnDefs}
-              rowData={this.props.rowData}
-              reactNext
-              animateRows
-              defaultColDef={{
-                resizable: true,
-                sortable: true,
-                suppressMenu: false,
-                editable: true,
-                headerComponentFramework: CustomHeader,
-                minWidth: 140,
-              }}
-              columnTypes={{
-                avatar: {},
-                address: {},
-                attachments: {},
-                checkbox: {},
-                country: {},
-                cover: {},
-                currency: {},
-                date: {},
-                default: {},
-                email: {},
-                member: {},
-                multipleSelect: {},
-                number: {},
-                percent: {},
-                phone: {},
-                primary: {},
-                progress: {},
-                rating: {},
-                singleSelect: {},
-                string: {},
-                text: {},
-                uid: {},
-                url: {},
-                vote: {},
-              }}
-              rowHeight={32}
-              {...this.props}
-            />
-          )}
-        </AgGridReact>
+          ref={innerRef}
+          componentWrappingElement="div"
+          columnDefs={this.props.columnDefs}
+          rowData={this.props.rowData}
+          reactNext
+          animateRows
+          defaultColDef={{
+            resizable: true,
+            sortable: true,
+            suppressMenu: false,
+            editable: true,
+            headerComponentFramework: CustomHeader,
+            minWidth: 140,
+          }}
+          columnTypes={{
+            avatar: {},
+            address: {},
+            attachments: {},
+            checkbox: {},
+            country: {},
+            cover: {},
+            currency: {},
+            date: {},
+            default: {},
+            email: {},
+            member: {},
+            multipleSelect: {},
+            number: {},
+            percent: {},
+            phone: {},
+            primary: {},
+            progress: {},
+            rating: {},
+            singleSelect: {},
+            string: {},
+            text: {},
+            uid: {},
+            url: {},
+            vote: {},
+          }}
+          rowHeight={32}
+          {...this.props}
+        />
       </div>
     );
   }
