dee493e413d2ddc55889d6158bfdbf56afdaa983
Andrea Vanini <andrea.vanini@uidu.org>
2019-12-10 13:08:42 +0100

    CHANGESET: eighty-kiwis-change. Export group renderer from table

---
+++ .changeset/eighty-kiwis-change.md
@@ -0,0 +1,6 @@
+---
+'@uidu/table': patch
+'@uidu/guidu-website': patch
+---
+
+Export group renderer from table
--- packages/data/table/src/index.ts
+++ packages/data/table/src/index.ts
@@ -1,4 +1,6 @@
 export * from './components/columns';
+export * from './components/groups';
+export * from './components/menu';
 export { default } from './components/Table';
 export * from './types';
 export * from './utils';
