411a15829e891c86090dc2dfad51e7f70730038d
Andrea Vanini <andrea.vanini@uidu.org>
2020-02-28 22:19:31 +0100

    docs(changeset): Shell shadow is now fixed and not sticky, maybe fixing ios flickering

---
+++ .changeset/grumpy-squids-itch.md
@@ -0,0 +1,7 @@
+---
+'@uidu/webpack-config': patch
+'@uidu/guidu-website': patch
+'@uidu/shell': patch
+---
+
+Shell shadow is now fixed and not sticky, maybe fixing ios flickering
--- website/webpack.config.js
+++ website/webpack.config.js
@@ -3,6 +3,9 @@ const runDevServer = require('@uidu/webpack-config/bin/dev.js');
 
 runDevServer({
   serverOptions: {
+    // for testing on your mobile, disable https
+    // and bind address to 0.0.0.0 using ifconfig 192.168.xxx.xxx:9000
+    // https: false,
     https: {
       key: fs.readFileSync(
         '/Users/andreavanini/Applications/uidu/config/certs/key.pem',
