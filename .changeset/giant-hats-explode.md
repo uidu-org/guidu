7bb581685bdb073a4d2bdced14a7adc73dd775a2
Andrea Vanini <andrea.vanini@uidu.org>
2020-02-29 08:53:57 +0100

    docs(changeset): FIX shadow on desktop

---
+++ .changeset/giant-hats-explode.md
@@ -0,0 +1,5 @@
+---
+'@uidu/shell': patch
+---
+
+FIX shadow on desktop
--- packages/navigation/shell/src/components/ShellBody/styled.tsx
+++ packages/navigation/shell/src/components/ShellBody/styled.tsx
@@ -16,18 +16,21 @@ export const ObserverComponent = styled.div`
 
 export const Shadow = styled.div<{ active: boolean }>`
   position: fixed;
+  height: 10px;
+  overflow: hidden;
   /* top: 0; */
   width: 100%;
   z-index: 1;
+  pointer-events: none;
 
   &::after {
     content: '';
     position: absolute;
     z-index: -1;
     width: 100%;
-    height: 100%;
+    height: 0;
     opacity: ${({ active }) => (active ? 1 : 0)};
     transition: opacity 0.3s ease-in-out;
-    box-shadow: 0px 1px 9px 1px #ccc;
+    box-shadow: 0px 1px 9px 1px rgba(204, 204, 204, 0.8);
   }
 `;
