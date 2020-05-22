a674930119cb2f0f324e4b36cb687d0ab93ad798
Andrea Vanini <andrea.vanini@uidu.org>
2020-05-22 22:01:12 +0200

    Re-enable certs for https

--- website/webpack.config.js
+++ website/webpack.config.js
@@ -5,15 +5,11 @@ runDevServer({
   serverOptions: {
     // for testing on your mobile, disable https
     // and bind address to 0.0.0.0 using ifconfig 192.168.xxx.xxx:9000
-    https: false,
-    // https: {
-    //   key: fs.readFileSync(
-    //     '/Users/andreavanini/Applications/uidu/config/certs/key.pem',
-    //   ),
-    //   cert: fs.readFileSync(
-    //     '/Users/andreavanini/Applications/uidu/config/certs/crt.pem',
-    //   ),
-    // },
+    // https: false,
+    https: {
+      key: fs.readFileSync('../../uidu/config/certs/key.pem'),
+      cert: fs.readFileSync('../../uidu/config/certs/crt.pem'),
+    },
   },
   webpackOptions: {
     resolve: {
