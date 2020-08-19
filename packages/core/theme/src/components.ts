eccb71a4e53bf9872d2f15a7321a29b73fb091ac
Andrea Vanini <andrea.vanini@uidu.org>
2020-08-19 22:30:50 +0200

    Fix theme build

--- packages/core/theme/src/components.ts
+++ packages/core/theme/src/components.ts
@@ -4,20 +4,15 @@
    Once the codemod is done and all the AK modules have been codeshifted, we delete index.js and rename this file to index + update all the imports
 */
 
-export { default as getTheme } from './utils/getTheme';
-export { default as themed } from './utils/themed';
-export { default as AtlaskitThemeProvider } from './components/AtlaskitThemeProvider';
-
-export { GlobalThemeTokens } from './types';
 export { default as Appearance } from './components/Appearance';
-
+export { default as AtlaskitThemeProvider } from './components/AtlaskitThemeProvider';
 // New API
-export { ResetThemeProps, ResetThemeTokens } from './components/Reset';
-export { ResetTheme, Reset } from './components/Reset';
-
+export { Reset, ResetTheme } from './components/Reset';
+export type { ResetThemeProps, ResetThemeTokens } from './components/Reset';
 export { default } from './components/Theme';
 export { withTheme } from './hoc';
-
-export { ThemeProp } from './utils/createTheme';
-
+export type { GlobalThemeTokens } from './types';
 export { createTheme } from './utils/createTheme';
+export type { ThemeProp } from './utils/createTheme';
+export { default as getTheme } from './utils/getTheme';
+export { default as themed } from './utils/themed';
