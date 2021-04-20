30f2f495f56681417e022e9b67476d6512587232
Andrea Vanini <andrea.vanini@uidu.org>
2021-04-20 13:49:00 +0200

    FIX #74

--- website/webpack.config.js
+++ website/webpack.config.js
@@ -1,16 +1,32 @@
 const runDevServer = require('@uidu/webpack-config/bin/dev.js');
 const fs = require('fs');
 
+let https = undefined;
+
+try {
+  const key = fs.readFileSync('../../uidu/config/certs/key.pem');
+  const cert = fs.readFileSync('../../uidu/config/certs/crt.pem');
+  https = {
+    key,
+    cert,
+  };
+} catch (err) {
+  console.log(
+    'Https is disabled, you should have a uidu certificate to run it with SSL',
+  );
+}
+
 runDevServer({
-  serverOptions: {
-    // for testing on your mobile, disable https
-    // and bind address to 0.0.0.0 using ifconfig 192.168.xxx.xxx:9000
-    // https: false,
-    https: {
-      key: fs.readFileSync('../../uidu/config/certs/key.pem'),
-      cert: fs.readFileSync('../../uidu/config/certs/crt.pem'),
-    },
-  },
+  ...(https
+    ? {
+        serverOptions: {
+          // for testing on your mobile, disable https
+          // and bind address to 0.0.0.0 using ifconfig 192.168.xxx.xxx:9000
+          // https: false,
+          https,
+        },
+      }
+    : {}),
   webpackOptions: {
     resolve: {
       alias: {
