7aaffda4da4d9329a4c9b57b0774390b0f95c7b1
Andrea Vanini <andrea.vanini@uidu.org>
2022-12-01 13:54:38 +0000

    docs(changeset): Fixed border radius

--- packages/forms/field-phone/docs/0-intro.tsx
+++ packages/forms/field-phone/docs/0-intro.tsx
@@ -2,7 +2,6 @@ import { code, Example, md, Props } from '@uidu/docs';
 import * as React from 'react';
 
 export default md`
-  ### Field Phone
   <p class="lead">Insert a phone number</p>
 
   FieldPhone uses \`react-phone-number-input\` to insert a phone number. It comes with intl support for countries phone number formatting.
