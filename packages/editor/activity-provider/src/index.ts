7c141de8bf19fd421be32436b7b16e3b1a0ddab7
Andrea Vanini <andrea.vanini@uidu.org>
2023-02-15 17:44:59 +0000

    FIX missing exports

--- packages/editor/activity-provider/src/index.ts
+++ packages/editor/activity-provider/src/index.ts
@@ -1,6 +1,6 @@
-import ActivityResource from './api/ActivityResource';
-import { ActivityProvider, ActivityItem } from './types';
-import { ActivityError } from './api/error';
+// import ActivityResource from './api/ActivityResource';
+import { ActivityItem, ActivityProvider } from './types';
+// import { ActivityError } from './api/error';
 
-export { ActivityResource, ActivityError };
+// export { ActivityResource, ActivityError };
 export type { ActivityProvider, ActivityItem };
