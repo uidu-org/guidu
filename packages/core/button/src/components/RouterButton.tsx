76e0b062a8425859994decfb759409ff2a6a5f8a
Andrea Vanini <andrea.vanini@uidu.org>
2024-09-06 12:54:50 +0200

    docs(changeset): Removed react-router-dom from button

--- packages/core/button/src/components/RouterButton.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { ButtonProps } from '../types';
-import Button from './Button';
-
-export default function RouterButton(props: ButtonProps) {
-  return <Button as={Link} to={props.to} {...props} />;
-}
--- packages/core/button/src/index.ts
+++ packages/core/button/src/index.ts
@@ -1,7 +1,6 @@
 import Button from './components/Button';
 
 export { default as ButtonGroup } from './components/ButtonGroup';
-export { default as RouterButton } from './components/RouterButton';
 export type { ButtonAppearances, ButtonProps } from './types';
 
 export default Button;
